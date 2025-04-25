c782b5a6bb8ecdc82cc55625dae23c65c4c0afa5
Gustavo Viana de Souza <gustavovianadesouzacontatos@gmail.com>
2025-04-25 12:41:23 -0300

    chat container padding fix

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -21,7 +21,7 @@ export default function ChatContainer() {
 
   return (
     <div className="flex flex-col flex-1 text-furia-white bg-gradient-to-t from-furia-black to-furia-gray overflow-y-auto" ref={chatWindowRef}>
-      <div className="px-5 py-10 flex flex-col gap-10">
+      <div className="px-5 pt-10 pb-15 flex flex-col gap-10">
         <ChatMessage message="FALA, PANTERA! 😎 Bem-vindo ao FURIA Chat o lugar certo para descobrir sobre o melhor time de CS com a gente!" time={"Agora"} role="assistant"/>
         <ChatMessage message="Quer saber do próximo jogo da FURIA, um fato irado sobre o FalleN, ou testar seus conhecimentos num quiz? 😜 Manda sua pergunta!" time={"Agora"} role="assistant"/>
         {messages.length > 0 && messages}
